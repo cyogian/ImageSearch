b1655f9af1fa28e311f64779fd27392eae93123b
Amar Nath Yogi <immortal2907@gmail.com>
2019-12-24 21:57:51 +0530

    Updates

---
+++ precache-manifest.f2f42436fafad0c1f71dcbe4f18a24bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c853f9a841b37eaceb02718a341f553d",
+    "url": "/ImageSearch/index.html"
+  },
+  {
+    "revision": "ef41a594532c3a0e0c5d",
+    "url": "/ImageSearch/static/css/main.70a906d6.chunk.css"
+  },
+  {
+    "revision": "fddaed2e2a87cb1d293c",
+    "url": "/ImageSearch/static/js/2.ff357761.chunk.js"
+  },
+  {
+    "revision": "ef41a594532c3a0e0c5d",
+    "url": "/ImageSearch/static/js/main.a9f81332.chunk.js"
+  },
+  {
+    "revision": "892761c16c85e1fcc7bc",
+    "url": "/ImageSearch/static/js/runtime~main.b9b5ebf8.js"
+  }
+]);
